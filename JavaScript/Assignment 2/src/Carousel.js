a1163842d2b1134d9e3f362a28cb18ee1861c40e
IshwarGautam <ishwargautam1@gmail.com>
2021-12-26 15:12:54 +0545

    Image Carousel: set fixed interval for transition and created multiple instances

--- JavaScript/Assignment 2/src/Carousel.js
+++ JavaScript/Assignment 2/src/Carousel.js
@@ -1,9 +1,9 @@
-//==================================================
-// Main function starts
-//==================================================
-
+//Constant variable
 const imageWidth = 960;
 
+//=================================
+// Main function (class) starts
+//=================================
 function Carousel(properties){
   this.wrapper = properties.imageContainer;
   this.wrapperId = properties.wrapperId;
@@ -29,15 +29,15 @@ function Carousel(properties){
   //==========================================
   for (let i=0; i<this.imageCount; i++){
     this.btn[i] = document.createElement('button');
-    this.btn[i].style.width = "20px";
-    this.btn[i].style.height = "20px";
-    this.btn[i].style.borderRadius = "50%";
-    this.btn[i].style.background = "gray";
-    this.btn[i].style.position = "relative";
-    this.btn[i].style.zIndex = "2";
-    this.btn[i].style.left =  (imageWidth/2) + (i * 20) - (this.imageCount * 20) + "px";
-    this.btn[i].style.bottom = "22px";
-    this.btn[i].style.cursor = "pointer";
+    this.btn[i].style.width = '20px';
+    this.btn[i].style.height = '20px';
+    this.btn[i].style.borderRadius = '50%';
+    this.btn[i].style.background = 'gray';
+    this.btn[i].style.position = 'relative';
+    this.btn[i].style.zIndex = '2';
+    this.btn[i].style.left =  (imageWidth/2) + (i * 20) - (this.imageCount * 20) + 'px';
+    this.btn[i].style.bottom = '22px';
+    this.btn[i].style.cursor = 'pointer';
 
     this.btn[i].onclick = function(){
       this.dx = i * imageWidth;
@@ -46,20 +46,20 @@ function Carousel(properties){
 
 
       for (let c=0; c<this.imageCount; c++){
-        this.btn[c].style.background = "gray";
+        this.btn[c].style.background = 'gray';
       }
 
-      this.btn[i].style.background = "green";
+      this.btn[i].style.background = 'green';
       this.turn = i+1;
 
       this.btn[i].onmouseout = function(){
-        this.btn[i].style.background = "green";
+        this.btn[i].style.background = 'green';
       }.bind(this);
     }.bind(this);
 
     this.btn[i].onmouseover = function(){
       this.getColor = this.btn[i].style.background;
-      this.btn[i].style.background = "#f5e";
+      this.btn[i].style.background = '#f5e';
 
       this.btn[i].onmouseout = function(){
         this.btn[i].style.background = this.getColor;
@@ -69,8 +69,8 @@ function Carousel(properties){
     this.wrapper.appendChild(this.btn[i]);
   }
 
-  
-  this.btn[0].style.background = "green"; //initial active indicator dot
+  //initial active indicator dot
+  this.btn[0].style.background = 'green'; 
 
   this.turn=1;
 
@@ -78,29 +78,30 @@ function Carousel(properties){
   //Implementation of next slider image
   //===================================
   this.nextBtn.onclick = function () {
-    if (this.counter == this.turn){ //counter detects the turn of indicator dot to be active
+    //counter detects the turn of indicator dot to be active
+    if (this.counter == this.turn){ 
       if (this.turn == this.imageCount){
         this.toggle = 0;
-        this.btn[imageCount-1].style.background = "gray";
-        this.btn[0].style.background = "green";
+        this.btn[imageCount-1].style.background = 'gray';
+        this.btn[0].style.background = 'green';
         this.turn=1;
       }  
       else {
-        this.btn[this.turn-1].style.background = "gray";
-        this.btn[this.turn].style.background = "green";
+        this.btn[this.turn-1].style.background = 'gray';
+        this.btn[this.turn].style.background = 'green';
         this.turn++;
       }
     }
 
-    //button is not clickable during transition
     if (this.dx % imageWidth == 0){
       this.interval = setInterval(() => {
         if (this.counter != this.imageCount){
           this.dx+=8;
           images[this.wrapperId].style.left = `-${this.dx}px`;
         }
-  
-        if (this.counter == this.imageCount){ // redirect to the beginning of the slider
+        
+        // redirect to the beginning of the slider
+        if (this.counter == this.imageCount){ 
           this.dx-=80;
           if (this.dx == 0){
             this.counter = 1;
@@ -123,13 +124,13 @@ function Carousel(properties){
     if (this.counter == this.turn){
       if(this.turn == 1) {
         this.toggle=1;
-        this.btn[0].style.background = "gray";
-        this.btn[this.imageCount-1].style.background = "green";
+        this.btn[0].style.background = 'gray';
+        this.btn[this.imageCount-1].style.background = 'green';
         this.turn = this.imageCount;
       } 
       else{
-        this.btn[this.turn-1].style.background = "gray";
-        this.btn[this.turn-2].style.background = "green";
+        this.btn[this.turn-1].style.background = 'gray';
+        this.btn[this.turn-2].style.background = 'green';
         this.turn--;
       } 
     }
@@ -141,7 +142,8 @@ function Carousel(properties){
           images[this.wrapperId].style.left = `-${this.dx}px`;
         }
         
-        if (this.counter==1){ //redirect to the end of the slider
+        //redirect to the end of the slider
+        if (this.counter==1){ 
           this.dx+=80
           if (this.dx >= (this.imageCount-1) * imageWidth){
             this.counter = this.imageCount;
@@ -157,19 +159,10 @@ function Carousel(properties){
     }  
   }.bind(this); 
 
+
   //======================================================
   // Changing the slider automatically at a fixed interval
   //======================================================
-  // if (this.prevBtn.click() || this.nextBtn.click()){
-  //   setTimeout(() => {
-  //     ChangedAutomatically();
-  //   }, 3000);
-  // }
-  // else{
-  //   ChangedAutomatically();
-  // }
-
-  
   this.toggle=0;
   this.automate = setInterval(() => {
     if (this.toggle==0){
@@ -187,15 +180,18 @@ function Carousel(properties){
 
     //code clearfix
     for (let b=0; b<this.imageCount; b++){
-      this.btn[b].style.background = "gray";
+      this.btn[b].style.background = 'gray';
     }
-    this.btn[this.turn-1].style.background = "green";
+    this.btn[this.turn-1].style.background = 'green';
     
-    setTimeout(this.automate, 3000);
-  },3000);
+    setTimeout(this.automate, this.holdTime);
+  },this.holdTime);
 }
 
 
+//================================
+// Creating instances of class
+//================================
 new Carousel({
   imageContainer:wrapper[0],
   wrapperId:0,
--- JavaScript/Assignment 2/src/style.js
+++ JavaScript/Assignment 2/src/style.js
@@ -7,42 +7,55 @@ let prevBtn = document.getElementsByClassName('previousBtn');
 let nextBtn = document.getElementsByClassName('nextBtn');
 
 for (let i=0; i< wrapper.length; i++){
-  wrapper[i].style.width = "960px";
-  wrapper[i].style.position = "relative";
-  wrapper[i].style.overflow = "hidden";
-  wrapper[i].style.transform = "translate(10%, 5%)";
 
-  images[i].style.height = "540px";
-  images[i].style.position = "relative";
+  //==============================================
+  // 1. Styling carousel-container (outermost div)
+  //==============================================
+  wrapper[i].style.width = '960px';
+  wrapper[i].style.position = 'relative';
+  wrapper[i].style.overflow = 'hidden';
+  wrapper[i].style.transform = 'translate(10%, 5%)';
 
+  //=================================================
+  // 2. Styling carousel wrapper that wrap each image
+  //=================================================
+  images[i].style.height = '540px';
+  images[i].style.position = 'relative';
+
+  //==============================
+  // 3. Styling cheveron icon left
+  //==============================
   prevBtn[i].style.fontSize = '60px';
   prevBtn[i].style.color = '#fc2';
-  prevBtn[i].style.position = "absolute";
-  prevBtn[i].style.bottom = "45%";
-  prevBtn[i].style.left = "10px";
-  prevBtn[i].style.zIndex = "2";
-  prevBtn[i].style.cursor = "pointer";
-  prevBtn[i].style.opacity = "50%";
+  prevBtn[i].style.position = 'absolute';
+  prevBtn[i].style.bottom = '45%';
+  prevBtn[i].style.left = '10px';
+  prevBtn[i].style.zIndex = '2';
+  prevBtn[i].style.cursor = 'pointer';
+  prevBtn[i].style.opacity = '50%';
   prevBtn[i].onmouseover = function(){
-    prevBtn[i].style.opacity = "100%";
+    prevBtn[i].style.opacity = '100%';
   }
   prevBtn[i].onmouseout = function(){
-    prevBtn[i].style.opacity = "50%";
+    prevBtn[i].style.opacity = '50%';
   }
 
+  //===============================
+  // 4. Styling cheveron icon right
+  //===============================
   nextBtn[i].style.fontSize = '60px';
   nextBtn[i].style.color = '#fc2';
-  nextBtn[i].style.position = "absolute";
-  nextBtn[i].style.bottom = "45%";
-  nextBtn[i].style.right = "10px";
-  nextBtn[i].style.zIndex = "2";
-  nextBtn[i].style.cursor = "pointer";
-  nextBtn[i].style.opacity = "50%";
+  nextBtn[i].style.position = 'absolute';
+  nextBtn[i].style.bottom = '45%';
+  nextBtn[i].style.right = '10px';
+  nextBtn[i].style.zIndex = '2';
+  nextBtn[i].style.cursor = 'pointer';
+  nextBtn[i].style.opacity = '50%';
   nextBtn[i].onmouseover = function(){
-    nextBtn[i].style.opacity = "100%";
+    nextBtn[i].style.opacity = '100%';
   }
   nextBtn[i].onmouseout = function(){
-    nextBtn[i].style.opacity = "50%";
+    nextBtn[i].style.opacity = '50%';
   }
 }
 
