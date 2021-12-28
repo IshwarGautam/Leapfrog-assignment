ee3e041c1e30c743f69b208b6d060b23b7f3c53d
IshwarGautam <ishwargautam1@gmail.com>
2021-12-28 20:31:16 +0545

    Asntm 4: first setup

---
+++ JavaScript/Assignment 4/src/carlane.js
@@ -0,0 +1,133 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); 
+}
+
+
+let index = 1;
+const laneCount = 3;
+const laneLength = 500;
+const carHeight = 100;
+
+const laneMap = {
+  0: "lane-left",
+  1: "lane-middle",
+  2: "lane-right"
+};
+
+function playKey(){
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowLeft") {
+      index--;
+      if (index < 0) index = 0;
+    } else if (event.code === "ArrowRight") {
+      index++;
+      if (index > laneCount - 1) index = laneCount - 1;
+    }
+  
+    const laneMapValue = laneMap[index];
+  
+    car.setAttribute("class", `car ${laneMapValue}`);
+  });
+}
+
+
+class Obstacle {
+  constructor(speed) {
+    this.index = getRandomInt(0, 3);
+    this.y = getRandomInt(-150, -200);
+    this.speed = speed;
+    this.passed = 0;
+  }
+
+  draw() {
+    this.element = document.createElement("div");
+
+    const laneMapValue = laneMap[this.index];
+
+    this.element.setAttribute("class", `car ${laneMapValue}`);
+    this.element.style.bottom = "auto";
+    this.element.style.top = this.y + "px";
+    this.element.style.transition = "none";
+
+    road.appendChild(this.element);
+  }
+
+  move() {
+    if (!this.passed){
+      this.y += this.speed;
+      this.element.style.top = this.y + "px";
+    }
+
+    this.collide();
+    
+    // console.log(this.y);
+    if (this.y >= laneLength) {
+      this.element.remove();
+      // this.y = -150;
+      this.passed = true;
+    }
+
+    if (this.y > laneLength-100){
+      this.index = -1;
+    }
+    
+  }
+
+  collide(){ 
+    if (this.y>laneLength - carHeight && index === this.index){
+      // window.cancelAnimationFrame(this.frameId);
+
+      
+      replayButton.style.display = 'block';
+    }
+  }
+}
+
+const obsArray = [];
+let speed = 5;
+let score = 0;
+let highScore = localStorage.getItem("highScore") || 0;
+
+// let frameId = window.requestAnimationFrame(playGame);
+function playGame(){
+  playButton.style.display = "none";
+  replayButton.style.display = "none";
+  playKey();
+
+  setInterval(() => {
+    speed += 0.1;
+    let obs = new Obstacle(speed);
+    obs.draw();
+    obsArray.push(obs);
+  
+    //set score
+    if (obsArray.length>2){
+      score = obsArray.length - 2;
+    }
+    else{
+      score = 0;
+    }
+  
+    if (score>highScore) {
+      highScore = score;
+      localStorage.setItem("highScore", highScore);
+    }
+  
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("highScore").innerHTML = highScore; 
+    
+  }, 1000);
+}
+
+playButton.addEventListener("click", playGame);
+replayButton.addEventListener("click", playGame);
+  
+
+function move() {
+  obsArray.forEach((obs) => obs.move());
+    requestAnimationFrame(move);
+}
+
+move();
---
+++ JavaScript/Assignment 4/src/style.js
@@ -0,0 +1,45 @@
+const road = document.getElementById("road");
+road.style.width = "300px";
+road.style.display = "flex";
+road.style.overflow = "hidden";
+road.style.position = "fixed";
+road.style.top = '50%';
+road.style.left = '50%';
+road.style.transform = 'translate(-50%, -50%)';
+
+let playButton = document.getElementById('play');
+playButton.style.background = "green";
+playButton.style.position = "absolute";
+playButton.style.width = "100px";
+playButton.style.height = "50px";
+playButton.style.color = "ivory";
+playButton.style.fontSize = "25px";
+playButton.style.cursor = "pointer";
+playButton.style.top = '50%';
+playButton.style.left = '50%';
+playButton.style.transform = 'translate(-50%, -50%)';
+road.appendChild(playButton);
+
+let replayButton = document.getElementById('replay');
+replayButton.style.background = "green";
+replayButton.style.position = "absolute";
+replayButton.style.width = "100px";
+replayButton.style.height = "50px";
+replayButton.style.color = "ivory";
+replayButton.style.fontSize = "25px";
+replayButton.style.cursor = "pointer";
+replayButton.style.top = '50%';
+replayButton.style.left = '50%';
+replayButton.style.transform = 'translate(-50%, -50%)';
+replayButton.style.display = 'none';
+road.appendChild(replayButton);
+
+// const car = document.getElementById("car");
+// car.style.width = '55px';
+// car.style.height = '100px';
+// car.style.position = 'absolute';
+// car.style.backgroundImage = `url('./images/car.png')`;
+// car.style.backgroundRepeat = 'no-repeat';
+// car.style.bottom = '10px';
+// car.style.transition = '0.4s';
+// car.style.backgroundPosition = 'center';
